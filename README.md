38eb479af133ee7ae39c09accd739c8c9336bcd4
aidilfahmiey <aidil.fahmiey@live.iium.edu.my>
2021-01-20 16:18:33 +0800

    update by aidil

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# OS_GroupProject
+# C++ implementations of FCFS for disc scheduling algorithms
+
+## Group Members
+
+| No  | Name                               | Matric No |
+| --- | ---------------------------------- | --------- |
+| 1   | Muhammad Aidil Fahmiey Bin Osman   | 1819369   |
+| 2   | Nur Aimi Binti Ahmad Nazri         | 1813394   |
+| 3   | Nik Aisyah Damia Binti Nik Suhaidi | 1910500   |
+
+## Algorithms
+
+- FCFS - fcfs.cpp
+
+## How to compile on Windows
+
+1. Make sure you have g++ compiler installed in your computer.
+2. Open command prompt terminal.
+3. Run this command to compile.
+
+```
+g++ -o FCFS_DISK_SCHEDULING FCFS_DISK_SCHEDULING.cpp
+```
+
+4. Run the program
+
+```
+FCFS_DISK_SCHEDULING.exe
+```
---
+++ src/FCFS_DISK_SCHEDULING.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <vector>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int numTrack, totalData, num1, num2, num3, num4, num5, num6, num7, num8, sum=0,choose,exit;
+    const int size = 20;
+    const int  numReq = 9;
+    const int startTrack = 53;
+    int data[size];
+	
+	cout<< "Please choose your case(1-4): ";
+	cin>>choose;
+	
+		switch(choose){
+		
+			case 1:
+			{
+				ifstream input;
+    			input.open("inputfile1.txt");
+    			
+    			ofstream output;
+    			output.open("outputfile1.txt");
+    
+    			for(int x=0; x<numReq; x++){
+    				input >> data[x];
+				}
+				numTrack = data[0];
+	
+				for(int y=0; y<numReq; y++){
+       				 if(data[y]>numTrack){
+           			 cout<<"Error, Unknown position "<<data[y]<<"\n";
+           			 return 0;
+       		 		}
+    			}
+	
+    			int temp = startTrack;
+    			cout << temp;
+   				 for (int i = 1; i < numReq; i++){
+       				 cout << " -> " << data[i] << ' ';
+       				 sum += abs(data[i] - temp);
+        		     temp = data[i];
+    			}
+    			 cout << '\n';
+   				 cout << "Case 1: " <<sum<<'\n';
+   				 output<<"Case 1: " <<sum<<endl;
+   			}
+   			break;
+   			 
+   		case 2:
+   			{
+				ifstream input;
+    			input.open("inputfile2.txt");
+    			
+    			ofstream output;
+    			output.open("outputfile2.txt");
+    
+    			for(int x=0; x<numReq; x++){
+    				input >> data[x];
+				}
+				numTrack = data[0];
+	
+				for(int y=0; y<numReq; y++){
+       				 if(data[y]>numTrack){
+           			 cout<<"Error, Unknown position "<<data[y]<<"\n";
+           			 return 0;
+        			}
+    			}
+	
+    			int temp = startTrack;
+    			cout << temp;
+   				 for (int i = 1; i < numReq; i++){
+       				 cout << " -> " << data[i] << ' ';
+       				 sum += abs(data[i] - temp);
+        		     temp = data[i];
+    			}
+    			 cout << '\n';
+   				 cout << "Case 2: " <<sum<< '\n';
+   				 output<<"Case 2: " <<sum<<endl;
+   			}
+   			break;
+   			
+   			case 3:
+   			{
+				ifstream input;
+    			input.open("inputfile3.txt");
+    			
+    			ofstream output;
+    			output.open("outputfile3.txt");
+    
+    			for(int x=0; x<numReq; x++){
+    				input >> data[x];
+				}
+				numTrack = data[0];
+	
+				for(int y=0; y<numReq; y++){
+       				 if(data[y]>numTrack){
+           			 cout<<"Error, Unknown position "<<data[y]<<"\n";
+           			 return 0;
+        			}
+    			}
+	
+    			int temp = startTrack;
+    			cout << temp;
+   				 for (int i = 1; i < numReq; i++){
+       				 cout << " -> " << data[i] << ' ';
+       				 sum += abs(data[i] - temp);
+        		     temp = data[i];
+    			}
+    			 cout << '\n';
+   				 cout << "Case 3: " <<sum<< '\n';
+   				 output<<"Case 3: " <<sum<<endl;
+   			}
+   			break;
+   			
+   			case 4:
+   			{
+				ifstream input;
+    			input.open("inputfile4.txt");
+    			
+    			ofstream output;
+    			output.open("outputfile4.txt");
+    
+    			for(int x=0; x<numReq; x++){
+    				input >> data[x];
+				}
+				numTrack = data[0];
+	
+				for(int y=0; y<numReq; y++){
+       				 if(data[y]>numTrack){
+           			 cout<<"Error, Unknown position "<<data[y]<<"\n";
+           			 return 0;
+        			}
+    			}
+	
+    			int temp = startTrack;
+    			cout << temp;
+   				 for (int i = 1; i < numReq; i++){
+       				 cout << " -> " << data[i] << ' ';
+       				 sum += abs(data[i] - temp);
+        		     temp = data[i];
+    			}
+    			 cout << '\n';
+   				 cout << "Case 4: " <<sum<< '\n';
+   				 output<<"Case 4: " <<sum<<endl;
+   			}
+   			break;			
+	};
+	
+    system("pause");
+    return 0;
+}
